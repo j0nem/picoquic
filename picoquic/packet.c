fd0f101764d1a23cfc23c5226e0fc5035d3ca36c
Your Name <johannes@jonesmedia.de>
2025-09-19 15:22:21 +0200

    Add todo comment - avoid idle timeout

--- picoquic/packet.c
+++ picoquic/packet.c
@@ -2600,6 +2600,7 @@ int picoquic_incoming_segment(
         if (cnx != NULL && cnx->cnx_state != picoquic_state_disconnected &&
             ph.ptype != picoquic_packet_version_negotiation) {
             cnx->nb_packets_received++;
+            // TODO MC: Make sure that this is reached in multicast setting, to avoid idle timeout
             cnx->latest_receive_time = current_time;
             /* Mark the sequence number as received */
             ret = picoquic_record_pn_received(cnx, ph.pc, ph.l_cid, ph.pn64, receive_time);
