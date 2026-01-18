ae3e4b813e4b7db3a0f4a7d84664fee4ea336ea6
Your Name <johannes@jonesmedia.de>
2026-01-18 19:56:31 +0100

    Activate gso again

--- multicast_sample/multicast_sender.c
+++ multicast_sample/multicast_sender.c
@@ -287,8 +287,8 @@ int multicast_sender_start(multicast_sender_ctx_t* sender_ctx, int* thread_ret, 
 
     if (server_ctx->is_local == 1) {
         param->force_localhost_src_ip = 1; /* Force localhost as src ip for multicast packets for local tests */
+        param->do_not_use_gso = 1; // CHECK MC: GSO deactivated currently due to issues with local interfaces, may be re-activated later?
     }
-    param->do_not_use_gso = 1; // CHECK MC: GSO deactivated currently due to issues with local interfaces, may be re-activated later?
 
     /* Start the background thread. */
     sender_ctx->thread_ctx = picoquic_start_custom_network_thread_ex(server_ctx->quic, param,
